e452454b5126e567f21747ec78ba5aa9a9336b43
ZhipYoung <1274254095@qq.com>
2021-01-03 15:26:04 +0800

    项目初始结构

---
+++ gumall-coupon/src/test/java/com/atyoung/gumall/gumallcoupon/GumallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atyoung.gumall.gumallcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GumallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gumall-member/src/test/java/com/atyoung/gumall/member/GumallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atyoung.gumall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GumallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
